0a0eda909e13d671c3f17717e2d3f31b98279c97
David Berger <d.berger@dontsniff.co.uk>
2020-04-29 12:18:39 +0200

    Random stuff

--- measuremeterdata/models.py
+++ measuremeterdata/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+class Continent(models.Model):
+    name = models.CharField(max_length=200)
+    ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 class Country(models.Model):
     name = models.CharField(max_length=200)
     code = models.CharField(max_length=3,blank=True,null=True)
+    category = models.ForeignKey(Continent, on_delete=models.CASCADE,blank=True,null=True)
+    link_worldometer = models.CharField(max_length=200,blank=True,null=True)
+    link_gov = models.CharField(max_length=200,blank=True,null=True)
+    comment = models.TextField(max_length=500,blank=True)
     ordering = ['name']
+
     def __str__(self):
         return self.name
 
@@ -28,6 +40,7 @@ class Measure(models.Model):
     start = models.DateField()
     end = models.DateField(null=True,blank=True)
     sources = models.TextField(max_length=300,blank=True)
+    comment = models.TextField(max_length=500,blank=True)
     isregional = models.BooleanField(default= False)
     ordering = ['country', 'type']
 
