4e410e53adb730147ca78487e27c563cab12bb5d
David Berger <d.berger@dontsniff.co.uk>
2020-04-30 15:57:17 +0200

    Updated README.md

--- measuremeterdata/admin.py
+++ measuremeterdata/admin.py
@@ -9,11 +9,13 @@ class MeasureTypeAdmin(admin.ModelAdmin):
     # a list of displayed columns name.
     list_display = ['name', 'category', 'isactive', 'comment']
     ordering = ['category', 'name']
+    search_fields = ['name']
 admin.site.register(MeasureType, MeasureTypeAdmin)
 
 
 class MeasureAdmin(admin.ModelAdmin):
     # a list of displayed columns name.
-    list_display = ['country', 'type', 'start', 'end']
+    list_display = ['country', 'type', 'comment', 'start', 'end']
     ordering = ['country', 'type']
+    autocomplete_fields = ['type']
 admin.site.register(Measure, MeasureAdmin)
