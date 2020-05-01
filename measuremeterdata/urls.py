1be955fd0e57987fd1860e7a9b0c81a90f00a1c5
David Berger <d.berger@dontsniff.co.uk>
2020-05-01 14:27:25 +0200

    Added by measure, sorting serializer

--- measuremeterdata/urls.py
+++ measuremeterdata/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'measures', views.MeasureViewSet)
+router.register(r'measuresbymeasure', views.MeasureByMeasureViewSet)
 router.register(r'countries', views.CountryViewSet)
 router.register(r'measuretypes', views.MeasureTypeViewSet)
 router.register(r'measurecatgories', views.MeasureCategoryViewSet)
