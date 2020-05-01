ae648ef7107679b47df66ccfc4270d7333e04ca4
David Berger <d.berger@dontsniff.co.uk>
2020-05-01 22:20:43 +0200

    White bar for none

--- measuremeterdata/models.py
+++ measuremeterdata/models.py
@@ -39,7 +39,7 @@ class MeasureType(models.Model):
 class Measure(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     type = models.ForeignKey(MeasureType, on_delete=models.CASCADE)
-    start = models.DateField()
+    start = models.DateField(null=True,blank=True)
     end = models.DateField(null=True,blank=True)
     partial = models.BooleanField(default=False)
     none = models.BooleanField(default=False)
