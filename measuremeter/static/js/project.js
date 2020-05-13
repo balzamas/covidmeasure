7a802dda87d078029645802354167b170e4d25d7
berger@init7.net <berger@init7.net>
2020-05-13 13:47:36 +0200

    Menu

--- measuremeter/static/js/project.js
+++ measuremeter/static/js/project.js
@@ -75,14 +75,13 @@
       $( document ).ready(function() {
           //document.getElementById("dateselect").valueAsDate = new Date();
 
+            console.log($( "#menu" ).length)
+          if ( $( "#menu" ).length ) {
+               document.getElementById('menu').innerHTML =
+              '<div class="border border-dark" style="padding-left: 10;padding-top: 10;padding-bottom: 10;margin-bottom: 10"><font size="3rem"><a href="/">Home</a> // <a href="/about/">About</a> // <a href="/timeline/">Timeline</a> // <a href="/euromap/">EuroMap</a>         </font>        </div>';
+           }
 
-           document.getElementById('menu').innerHTML =
-          '<div class="border border-dark" style="padding-left: 10;padding-top: 10;padding-bottom: 10;margin-bottom: 10"><font size="3rem"><a href="/">Home</a> // <a href="/about/">About</a> // <a href="/timeline/">Timeline</a> // <a href="/euromap/">EuroMap</a>         </font>        </div>';
-
-          menu = document.getElementById('title_bar');
-          console.log(menu)
-          if(typeof menu !== null && menu !== 'undefined' )
-          {
+          if ( $( "#title_bar" ).length ) {
               document.getElementById('title_bar').innerHTML =
               '<div class="border border-dark" style="padding-left: 10;padding-top: 10;padding-bottom: 10;margin-bottom: 10"><font size="3rem">Contact: d.berger@dontsniff.co.uk // Twitter: <a href="https://twitter.com/BergerWthur">@BergerWthur</a>// <a href="/measuremeterdata/">REST Endpoints</a>  // <a href="https://github.com/balzamas/covidmeasure/">Project on GitHub</a><br>         Please contact me if you have corrections/find wrong data. </font>        </div>';
           }
