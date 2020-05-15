69a4c670810305c3877bdb8ac1f0aa5a9dacdd7c
David Berger <d.berger@dontsniff.co.uk>
2020-05-15 11:04:27 +0200

    gfx workover

---
+++ config/views.py
@@ -0,0 +1,11 @@
+from measuremeterdata.models import Measure, Country, MeasureType, MeasureCategory
+from django.shortcuts import get_object_or_404, render
+
+def render_country(request, country_name):
+    item = get_object_or_404(Country, code=country_name)
+    return render(request, 'pages/country.html', {'item': item })
+
+def render_euromap(request, measure_id):
+    item = get_object_or_404(MeasureType, pk=measure_id)
+    return render(request, 'pages/euromap.html', {'item': item })
+
