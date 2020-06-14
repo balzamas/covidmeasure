f9eebdd3e59910244d33b86cf145b93f800f04bd
David Berger <d.berger@dontsniff.co.uk>
2020-06-14 10:47:21 +0200

    Fix order

--- measuremeterdata/views.py
+++ measuremeterdata/views.py
@@ -50,7 +50,7 @@ def get_queryset(self):
         if start:
             measures.filter(start__lte=start)  # returned queryset filtered by ids
         if end:
-            measures.filter(end__gt=end)  # returned queryset filtered by ids
+            measures.filter(end__gte=end)  # returned queryset filtered by ids
         if levels:
             measures.filter(level__in=levels)  # returned queryset filtered by ids
 
@@ -98,7 +98,7 @@ class MeasureViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(Q(start__lte=start)|Q(start__isnull=True))
 
         if end != None:
-            queryset = queryset.filter(Q(end__gt=end)|Q(end__isnull=True))
+            queryset = queryset.filter(Q(end__gte=end)|Q(end__isnull=True))
 
         if levels != None and levels != '' and types != ',':
             level_params = []
