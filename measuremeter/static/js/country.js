b50ef2abfb969573d021846ce4384cdbd39b7bff
David Berger <d.berger@dontsniff.co.uk>
2020-08-06 12:26:10 +0200

    better visuals country

--- measuremeter/static/js/country.js
+++ measuremeter/static/js/country.js
@@ -1,3 +1,8 @@
+     	var config_cases;
+     	var config_death;
+     	var avg_desc;
+     	var avg_peak_desc;
+
       function LoadCountryData(country_id)
       {
           var data = $.ajax({
@@ -14,19 +19,15 @@
           document.getElementById('gov').innerHTML = '<p><a href="' + jsonData[0].link_gov + '" target="_blank">Link Government</a></p>' ;
           if (Number(jsonData[0].average_death_per_day)>0)
           {
-            $('#avg_desc').show();
-            $('#avg_peak_desc').show();
             $('#source_death').show();
             $('#deaths_description').show();
-            document.getElementById('avg_desc').innerHTML = "<p>Deaths Average: " + jsonData[0].avg_desc +"</p>";
-            document.getElementById('avg_peak_desc').innerHTML = "<p>Deaths Peak: " + jsonData[0].avg_peak_desc+"</p>";
+            avg_desc = "Deaths Average: " + jsonData[0].avg_desc +"";
+            avg_peak_desc = "Deaths Peak: " + jsonData[0].avg_peak_desc+"";
             document.getElementById('source_death').innerHTML = "<p>Source total deaths: <a href='" + jsonData[0].source_death +"'> Link</a></p>";
 
           }
           else
           {
-            $('#avg_desc').hide();
-            $('#avg_peak_desc').hide();
             $('#source_death').hide();
             $('#deaths_description').hide();
            }
@@ -125,6 +126,191 @@
 
       }
 
+      function drawLineChart(country, avg_values, startdate, endate)
+      {
+
+          lastdate_x = formatDate(endate);
+          firstdate_x = formatDate(startdate);
+
+          var data = $.ajax({
+          url: "/measuremeterdata/casesdeaths/?country="+country+"&date_after=2020-02-20&date_before="+lastdate_x,
+          dataType: "json",
+          async: false
+          }).responseText;
+          var jsonData = JSON.parse(data);
+
+        var dataset_cases = new Array()
+        var dataset_deaths = new Array()
+        var label_array = new Array()
+
+        var dataset_data_cases = new Array()
+        var dataset_data_deaths = new Array()
+
+        var dataset_data_total = new Array()
+        var dataset_data_avg = new Array()
+        var dataset_data_peak = new Array()
+
+        rowsCases = new Array();
+        rowsDeaths = new Array();
+
+        $.each(jsonData, function(id, line) {
+
+            label_array.push(line['date'])
+
+            dataset_data_cases.push(line['cases']);
+
+              if (Number(avg_values[0] > -1))
+              {
+                dataset_data_deaths.push(line['deaths']);
+                dataset_data_total.push(line['deathstotal']);
+                dataset_data_avg.push(Number(avg_values[0]));
+                dataset_data_peak.push(Number(avg_values[1]));
+              }
+              else
+              {
+                dataset_data_deaths.push(line['deaths'])
+              }
+        });
+
+        color = '#ff0000'
+        dataset_cases.push({"label": "Cases", fill: false, backgroundColor: color, borderColor: color, data: dataset_data_cases})
+
+        color = '#ff0000'
+        dataset_deaths.push({"label": "Covid", fill: false, backgroundColor: color, borderColor: color, data: dataset_data_deaths})
+        if (Number(avg_values[0] > 0))
+        {
+            color = '#0000ff'
+            dataset_deaths.push({"label": "Total", fill: false, backgroundColor: color, borderColor: color, data: dataset_data_total})
+        }
+
+
+            config_cases = {
+                type: 'line',
+
+                data: {
+                    labels: label_array,
+                    datasets: dataset_cases
+                },
+                options: {
+                    legend:{display: true,labels:{fontSize:20}},
+                    responsive: true,
+                    title: {
+                        display: true,
+                        text: 'Positive tests',
+                        fontSize: 25
+
+                    },
+                    tooltips: {
+                        mode: 'index',
+                        intersect: false,
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: true
+                    },
+                    scales: {
+                        x: {
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Day'
+                            }
+                        },
+                        y: {
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Positive tests'
+                            }
+                        }
+                    },
+                    }
+                    }
+
+            annotations = new Array();
+            if (avg_values[0] > 0)
+            {
+               console.log("......")
+                annotations.push({
+						type: 'line',
+						mode: 'horizontal',
+						scaleID: 'y-axis-0',
+						value: avg_values[0],
+						borderColor: 'black',
+						borderWidth: 5,
+						label: {
+							backgroundColor: 'red',
+							content: avg_desc,
+							fontSize: 19,
+							enabled: true
+						},
+					})
+				annotations.push({
+						type: 'line',
+						mode: 'horizontal',
+						scaleID: 'y-axis-0',
+						value: avg_values[1],
+						borderColor: 'black',
+						borderWidth: 5,
+						label: {
+							backgroundColor: 'red',
+							content: avg_peak_desc,
+                            fontSize: 19,
+							enabled: true
+						},
+					})
+
+            }
+
+            config_death = {
+                type: 'line',
+
+                data: {
+                    labels: label_array,
+                    datasets: dataset_deaths
+                },
+                options: {
+                    legend:{display: true,labels:{fontSize:20}},
+                    responsive: true,
+                    title: {
+                        display: true,
+                        text: 'Deaths per day',
+                        fontSize: 25
+
+                    },
+                    tooltips: {
+                        mode: 'index',
+                        intersect: false,
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: true
+                    },
+                    scales: {
+                        x: {
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Day'
+                            }
+                        },
+                        y: {
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Covid deaths per day'
+                            }
+                        }
+                    },
+                    annotation: {
+					 annotations: annotations
+				}
+
+                 }
+            }
+      }
+
+
 
 
       function LoadPanelsFiltered()
@@ -134,6 +320,12 @@
                 avg_values = LoadCountryData($('#countries_dd').dropdown('get value'));
                 var datesft = drawTimeline(2,$('#countries_dd').dropdown('get value'));
                 drawLineChart($('#countries_dd').dropdown('get value'),avg_values, datesft[0], datesft[1]);
+
+			    var ctx = document.getElementById('casesChart').getContext('2d');
+			    window.myLine = new Chart(ctx, config_cases);
+
+  			    var ctx = document.getElementById('deathChart').getContext('2d');
+			    window.myLine = new Chart(ctx, config_death);
              }
       }
 
