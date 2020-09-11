df3d2b692bd554fcd406f8a42406ca9d4026116e
David Berger <d.berger@dontsniff.co.uk>
2020-09-12 01:43:24 +0200

    fix SG

--- measuremeterdata/management/commands/importcases_sg.py
+++ measuremeterdata/management/commands/importcases_sg.py
@@ -35,6 +35,7 @@ def set_incidence(last_numbers, bezirk, date):
         total += cases
 
     ftdays = total / bezirk.population * 100000
+    print(ftdays)
 
     try:
         cd_existing = CHCases.objects.get(canton=bezirk, date=date)
@@ -51,71 +52,76 @@ class Command(BaseCommand):
       url="https://www.sg.ch/ueber-den-kanton-st-gallen/statistik/covid-19/_jcr_content/Par/sgch_downloadlist/DownloadListPar/sgch_download.ocFile/KantonSG_C19-Faelle_download.csv"
 
       with requests.Session() as s:
-        download = s.get(url)
-        decoded_content = download.content.decode('latin-1')
 
-        cr = csv.reader(decoded_content.splitlines(), delimiter=';')
-        my_list = list(cr)
+          download = s.get(url)
 
-        last_numbers_sg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
-        last_numbers_wil = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
-        last_numbers_rorschach = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
-        last_numbers_rheintal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
-        last_numbers_werdenberg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
-        last_numbers_sarganserland = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
-        last_numbers_seegaster = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
-        last_numbers_toggenburg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+          decoded_content = download.content.decode('latin-1')
 
-        count = 0
-        for row in my_list:
-          if (count > 1):
-            date = date.fromisoformat(row[0])
-            # St. Gallen
-            bezirk = CHCanton.objects.get(swisstopo_id=1721)
-            last_numbers_sg.append(int(row[4]))
-            last_numbers_sg.pop(0)
-            set_incidence(last_numbers_sg, bezirk, date)
+          cr = csv.reader(decoded_content.splitlines(), delimiter=';')
+          my_list = list(cr)
 
-            # Rorschach
-            bezirk = CHCanton.objects.get(swisstopo_id=1722)
-            last_numbers_rorschach.append(int(row[6]))
-            last_numbers_rorschach.pop(0)
-            set_incidence(last_numbers_rorschach, bezirk, date)
+          last_numbers_sg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+          last_numbers_wil = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+          last_numbers_rorschach = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+          last_numbers_rheintal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+          last_numbers_werdenberg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+          last_numbers_sarganserland = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+          last_numbers_seegaster = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+          last_numbers_toggenburg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
 
-            # Rheintal
-            bezirk = CHCanton.objects.get(swisstopo_id=1723)
-            last_numbers_rheintal.append(int(row[8]))
-            last_numbers_rheintal.pop(0)
-            set_incidence(last_numbers_rheintal, bezirk, date)
+          print("HANSA")
 
-            # Werdenberg
-            bezirk = CHCanton.objects.get(swisstopo_id=1724)
-            last_numbers_werdenberg.append(int(row[10]))
-            last_numbers_werdenberg.pop(0)
-            set_incidence(last_numbers_werdenberg, bezirk, date)
+          count = 0
+          for row in my_list:
+              print(count)
+              if (count > 6):
+                date_tosave = date.fromisoformat(row[0])
+                # St. Gallen
+                bezirk = CHCanton.objects.get(swisstopo_id=1721)
+                last_numbers_sg.append(int(row[4]))
+                last_numbers_sg.pop(0)
+                set_incidence(last_numbers_sg, bezirk, date_tosave)
 
-            # Sarganserland
-            bezirk = CHCanton.objects.get(swisstopo_id=1725)
-            last_numbers_sarganserland.append(int(row[12]))
-            last_numbers_sarganserland.pop(0)
-            set_incidence(last_numbers_sarganserland, bezirk, date)
+                # Rorschach
+                bezirk = CHCanton.objects.get(swisstopo_id=1722)
+                last_numbers_rorschach.append(int(row[6]))
+                last_numbers_rorschach.pop(0)
+                set_incidence(last_numbers_rorschach, bezirk, date_tosave)
 
-            # See-Gaster
-            bezirk = CHCanton.objects.get(swisstopo_id=1726)
-            last_numbers_seegaster.append(int(row[14]))
-            last_numbers_seegaster.pop(0)
-            set_incidence(last_numbers_seegaster, bezirk, date)
+                # Rheintal
+                bezirk = CHCanton.objects.get(swisstopo_id=1723)
+                last_numbers_rheintal.append(int(row[8]))
+                last_numbers_rheintal.pop(0)
+                set_incidence(last_numbers_rheintal, bezirk, date_tosave)
 
-            # Toggenburg
-            bezirk = CHCanton.objects.get(swisstopo_id=1727)
-            last_numbers_toggenburg.append(int(row[16]))
-            last_numbers_toggenburg.pop(0)
-            set_incidence(last_numbers_toggenburg, bezirk, date)
+                # Werdenberg
+                bezirk = CHCanton.objects.get(swisstopo_id=1724)
+                last_numbers_werdenberg.append(int(row[10]))
+                last_numbers_werdenberg.pop(0)
+                set_incidence(last_numbers_werdenberg, bezirk, date_tosave)
 
-            # Wil
-            bezirk = CHCanton.objects.get(swisstopo_id=1728)
-            last_numbers_wil.append(int(row[18]))
-            last_numbers_wil.pop(0)
-            set_incidence(last_numbers_wil, bezirk, date)
+                # Sarganserland
+                bezirk = CHCanton.objects.get(swisstopo_id=1725)
+                last_numbers_sarganserland.append(int(row[12]))
+                last_numbers_sarganserland.pop(0)
+                set_incidence(last_numbers_sarganserland, bezirk, date_tosave)
 
+                # See-Gaster
+                bezirk = CHCanton.objects.get(swisstopo_id=1726)
+                last_numbers_seegaster.append(int(row[14]))
+                last_numbers_seegaster.pop(0)
+                set_incidence(last_numbers_seegaster, bezirk, date_tosave)
 
+                # Toggenburg
+                bezirk = CHCanton.objects.get(swisstopo_id=1727)
+                last_numbers_toggenburg.append(int(row[16]))
+                last_numbers_toggenburg.pop(0)
+                set_incidence(last_numbers_toggenburg, bezirk, date_tosave)
+
+                # Wil
+                bezirk = CHCanton.objects.get(swisstopo_id=1728)
+                last_numbers_wil.append(int(row[18]))
+                last_numbers_wil.pop(0)
+                set_incidence(last_numbers_wil, bezirk, date_tosave)
+
+              count += 1
