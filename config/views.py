780aae6f19ab342162b610837da7a017aea580bc
David Berger <d.berger@dontsniff.co.uk>
2020-09-21 00:46:03 +0200

    5% testing

--- config/views.py
+++ config/views.py
@@ -32,21 +32,59 @@ def ranking(request):
         last_date = cases[0].date
         last_prev = cases[0].cases_past7days
         past_date_tocheck = last_date - timedelta(days=7)
+        past_past_date_tocheck = past_date_tocheck - timedelta(days=7)
 
         case_7days_before = CHCases.objects.get(canton=canton, date=past_date_tocheck)
+        case_7days_before_before = CHCases.objects.get(canton=canton, date=past_past_date_tocheck)
+
 
         if (case_7days_before.cases_past7days > 0):
             tendency = ((cases[0].cases_past7days * 100 / case_7days_before.cases_past7days) - 100)
         else:
             tendency = ((cases[0].cases_past7days * 100 / 1) - 100)
 
-        score = 0 - cases[0].cases_past7days - (tendency / 10)
+        if (case_7days_before_before.cases_past7days > 0):
+            tendency_7daysbefore = ((case_7days_before.cases_past7days * 100 / case_7days_before_before.cases_past7days) - 100)
+        else:
+            tendency_7daysbefore = ((case_7days_before.cases_past7days * 100 / 1) - 100)
 
-        canton_toadd = {"name": canton.name, "score": int(score), "date": last_date, "cur_prev": last_prev,
-                        "tendency": int(tendency)}
+
+        score = 0 - cases[0].cases_past7days - (tendency / 10)
+        score_7days_before = 0 - case_7days_before.cases_past7days - (tendency_7daysbefore / 10)
+
+        if (score > score_7days_before):
+            arrow = "arrow circle up green"
+        elif (score == score_7days_before):
+            arrow = "arrow circle left orange"
+        else:
+            arrow = "arrow circle down red"
+
+        canton_toadd = {"name": canton.name, "score": int(score), "score_before": int(score_7days_before),
+                        "date": last_date,
+                        "cur_prev": last_prev, "tendency": int(tendency),
+                        "cur_prev7": case_7days_before.cases_past7days, "tendency7": int(tendency_7daysbefore), "icon": arrow}
+
         canton_vals.append(canton_toadd)
 
-    scores = sorted(canton_vals, key=lambda i: i['score'],reverse=True)
+    scores = sorted(canton_vals, key=lambda i: i['score_before'],reverse=True)
+    rank = 1
+    for score in scores:
+        score["rank_old"] = rank
+        rank += 1
+
+    scores = sorted(scores, key=lambda i: i['score'],reverse=True)
+    rank = 1
+    for score in scores:
+        score["rank"] = rank
+        score["rank_diff"] = score["rank_old"] - rank
+        rank += 1
+        if (score["rank"] < score["rank_old"]):
+            score["rank_icon"] = "arrow circle up green"
+        elif (score["rank"] == score["rank_old"]):
+            score["rank_icon"] = "arrow circle left orange"
+        else:
+            score["rank_icon"] = "arrow circle down red"
+
 
     template = loader.get_template('pages/ranking.html')
     context = {
@@ -65,21 +103,56 @@ def ranking14_calc(cantons):
         last_date = cases[0].date
         last_prev = cases[0].cases_past14days
         past_date_tocheck = last_date - timedelta(days=14)
+        past_past_date_tocheck = past_date_tocheck - timedelta(days=14)
 
-        case_7days_before = CHCases.objects.get(canton=canton, date=past_date_tocheck)
+        case_14days_before = CHCases.objects.get(canton=canton, date=past_date_tocheck)
+        case_14days_before_before = CHCases.objects.get(canton=canton, date=past_past_date_tocheck)
 
-        if (case_7days_before.cases_past14days > 0):
-            tendency = ((cases[0].cases_past14days * 100 / case_7days_before.cases_past14days) - 100)
+        if (case_14days_before.cases_past14days > 0):
+            tendency = ((cases[0].cases_past14days * 100 / case_14days_before.cases_past14days) - 100)
         else:
             tendency = ((cases[0].cases_past14days * 100 / 1) - 100)
 
-        score = 0 - cases[0].cases_past14days - (tendency / 5)
+        if (case_14days_before_before.cases_past14days > 0):
+            tendency_14daysbefore = ((case_14days_before.cases_past14days * 100 / case_14days_before_before.cases_past14days) - 100)
+        else:
+            tendency_14daysbefore = ((case_14days_before.cases_past14days * 100 / 1) - 100)
 
-        canton_toadd = {"name": canton.name, "score": int(score), "date": last_date, "cur_prev": last_prev,
-                        "tendency": int(tendency)}
+        score = 0 - cases[0].cases_past14days - (tendency / 5)
+        score_14days_before = 0 - case_14days_before.cases_past14days - (tendency_14daysbefore / 10)
+
+        if (score > score_14days_before):
+            arrow = "arrow circle up green"
+        elif (score == score_14days_before):
+            arrow = "arrow circle left orange"
+        else:
+            arrow = "arrow circle down red"
+
+        canton_toadd = {"name": canton.name, "score": int(score),"score_before": int(score_14days_before),
+                        "date": last_date, "cur_prev": last_prev,
+                        "tendency": int(tendency), "icon": arrow}
         canton_vals.append(canton_toadd)
 
-    return sorted(canton_vals, key=lambda i: i['score'],reverse=True)
+    scores = sorted(canton_vals, key=lambda i: i['score_before'],reverse=True)
+    rank = 1
+    for score in scores:
+        score["rank_old"] = rank
+        rank += 1
+
+    scores = sorted(scores, key=lambda i: i['score'],reverse=True)
+    rank = 1
+    for score in scores:
+        score["rank"] = rank
+        score["rank_diff"] = score["rank_old"] - rank
+        rank += 1
+        if (score["rank"] < score["rank_old"]):
+            score["rank_icon"] = "arrow circle up green"
+        elif (score["rank"] == score["rank_old"]):
+            score["rank_icon"] = "arrow circle left orange"
+        else:
+            score["rank_icon"] = "arrow circle down red"
+
+    return scores
 
 def ranking14(request):
 
