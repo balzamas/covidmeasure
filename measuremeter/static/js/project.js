99a0db9c1e0a68aa72ee37e4b56d76f86c512841
berger@init7.net <berger@init7.net>
2020-10-08 09:58:19 +0200

    Berna

--- measuremeter/static/js/project.js
+++ measuremeter/static/js/project.js
@@ -17,6 +17,21 @@
                 </div> `
            }
 
+           if ( $( "#menu_ch" ).length ) {
+               document.getElementById('menu').innerHTML = `
+               <div style="padding-left: 10;padding-top: 10;padding-bottom: 10;padding-right: 10;margin-bottom: 10">
+               <div class="ui seven item inverted huge menu">
+                  <a class="item" href="/">Home</a>
+                  <a class="item" href="/cantons/">Measures</a>
+                  <a class="item" href="/chrisk/">Risk calc.</a>
+                  <a class="item" href="/chmaps/">Map measures</a>
+                  <a class="item" href="/districts/">Map cases<br>Districts & Cantons</a>
+                  <a class="item" href="/ranking14/">Ranking</a>
+                  <a class="item" href="/">Europe</a>
+                  </div>
+                </div> `
+           }
+
           if ( $( "#title_bar" ).length ) {
               document.getElementById('title_bar').innerHTML = `
               <div class="ui segment">
