44be4b1a7b767819c829217fc6c534d47ee2baab
dbe_macix <d.berger@dontsniff.co.uk>
2020-10-25 14:35:34 +0100

    BEL App

--- riskcalc/admin.py
+++ riskcalc/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from riskcalc.models import BELCases, BELProvince, BELAgeGroups
 
 # Register your models here.
+admin.site.register(BELProvince)
+admin.site.register(BELAgeGroups)
+
+class BELCasesAdmin(admin.ModelAdmin):
+    list_display = ['province', 'date']
+    list_filter = ('province','date')
+admin.site.register(BELCases, BELCasesAdmin)
---
+++ riskcalc/management/commands/copy_from_old_app.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand, CommandError
+from riskcalc.models import BELProvince, BELAgeGroups
+from measuremeterdata.models import  BELProvince as BELProvince_old
+from measuremeterdata.models import  BELAgeGroups as BELAgeGroups_old
+
+import os
+import csv
+import datetime
+import requests
+import pandas as pd
+from datetime import date, timedelta
+import urllib.request, json
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+
+        for x in BELProvince_old.objects.all().iterator():
+            cd = BELProvince()
+            cd.name = x.name
+            cd.name_source = x.name_source
+            cd.population = x.population
+            cd.hasc = x.hasc
+
+            cd.save()
+
+        for x in BELAgeGroups_old.objects.all().iterator():
+            cd = BELAgeGroups()
+            cd.name = x.name
+            cd.population = x.population
+            cd.save()
+
+
