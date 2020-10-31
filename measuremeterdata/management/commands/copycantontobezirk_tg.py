bf69f2939b20ae36f565bedf2890d4a40ccb5300
David Berger <d.berger@dontsniff.co.uk>
2020-10-31 16:59:08 +0100

    Added Thurgau

---
+++ measuremeterdata/management/commands/copycantontobezirk_tg.py
@@ -0,0 +1,42 @@
+from measuremeterdata.models import Country, MeasureCategory, MeasureType, Measure, Continent, CasesDeaths, CHCanton, CHCases
+from django.core.management.base import BaseCommand, CommandError
+import datetime
+from datetime import date, timedelta
+
+def saveit(bezirk_id, ftdays, date):
+    bezirk = CHCanton.objects.get(id=bezirk_id)
+    try:
+        cd_existing = CHCases.objects.get(canton=bezirk, date=date)
+        cd_existing.incidence_past14days = ftdays
+        cd_existing.save()
+    except CHCases.DoesNotExist:
+        cd = CHCases(canton=bezirk, incidence_past14days=ftdays, date=date)
+        cd.save()
+
+def get_start_date(year, week):
+    d = datetime.date(year, 1, 1)
+    if (d.weekday() <= 3):
+        d = d - timedelta(d.weekday())
+    else:
+        d = d + timedelta(7 - d.weekday())
+    dlt = timedelta(days=(week - 1) * 7)
+    return d + dlt
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        #BE
+
+        start_be = get_start_date(2020,30)
+        for cases in CHCases.objects.filter(canton=52).order_by('date'):
+            if (cases.date < start_be):
+                saveit(2011, cases.incidence_past14days, cases.date)
+                saveit(2012, cases.incidence_past14days, cases.date)
+                saveit(2013, cases.incidence_past14days, cases.date)
+                saveit(2014, cases.incidence_past14days, cases.date)
+                saveit(2015, cases.incidence_past14days, cases.date)
+
+
+
+
+
+        print(start_fr)
