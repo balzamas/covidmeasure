2ac97c06dba530fd167fab043a93fcb62c7616db
David Berger <d.berger@dontsniff.co.uk>
2020-11-15 21:56:08 +0100

    Bar chart races

--- measuremeterdata/management/commands/generate_csv.py
+++ measuremeterdata/management/commands/generate_csv.py
@@ -36,3 +36,26 @@ class Command(BaseCommand):
         for day  in cases:
             f.write(f"{day.date},{day.country.name},{day.cases_past14days}\n")
         f.close()
+
+        f = open("measuremeter/static/csv/countries14_death_start.csv", "w")
+        start_date = date(2020, 3, 20)
+        cases = CasesDeaths.objects.filter(country__continent=1,date__gte=start_date).order_by("date")
+        f.write("Date,Country,Value\n")
+        for day  in cases:
+            f.write(f"{day.date},{day.country.name},{day.deaths_past14days}\n")
+        f.close()
+
+        f = open("measuremeter/static/csv/countries14_positivity_start.csv", "w")
+        start_date = date(2020, 2, 27)
+        cases = CasesDeaths.objects.filter(country__continent=1,date__gte=start_date).order_by("date")
+        f.write("Date,Country,Value\n")
+        for day  in cases:
+            if day.positivity == None:
+                pos = 0
+            else:
+                pos = day.positivity
+
+            f.write(f"{day.date},{day.country.name},{pos}\n")
+        f.close()
+
+
