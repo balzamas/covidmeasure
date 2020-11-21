1d7bb8b433ca8ac716cf0abe368cf2ccfe23c14e
David Berger <d.berger@dontsniff.co.uk>
2020-11-21 20:31:56 +0100

    Re!

---
+++ measuremeterdata/management/commands/zero_to_null_r0.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from measuremeterdata.models.models import Country, MeasureCategory, MeasureType, Measure, Continent, CasesDeaths
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+
+        for x in CasesDeaths.objects.all():
+            if (x.r0peak == 0):
+                x.r0peak = None
+
+            if (x.r0low == 0):
+                x.r0low = None
+                
+            if (x.r0median == 0):
+                x.r0median = None
+
+            x.save()
