389518a3147a414cf28d2ed2d9a7e258827603b2
David Berger <d.berger@dontsniff.co.uk>
2020-12-01 21:57:15 +0100

    Belgium barchart

--- measuremeterdata/management/commands/generate_csv.py
+++ measuremeterdata/management/commands/generate_csv.py
@@ -1,5 +1,6 @@
 from measuremeterdata.models.models_ch import CHCanton, CHCases
 from measuremeterdata.models.models import CasesDeaths
+from riskcalc.models import BELCases
 from django.core.management.base import BaseCommand, CommandError
 from datetime import date
 
@@ -19,6 +20,24 @@ class Command(BaseCommand):
         f.write("Date,Canton,Value\n")
         for day  in cases:
             f.write(f"{day.date},{day.canton.name},{day.incidence_past14days}\n")
+        f.close()
+
+        f = open("measuremeter/static/csv/bel14_start.csv", "w")
+        start_date = date(2020, 2, 28)
+        cases = BELCases.objects.filter(date__gte=start_date).order_by("date")
+        f.write("Date,Province,Value\n")
+        for day  in cases:
+            total_cases = day.cases0_9 + day.cases10_19 + day.cases20_29 + day.cases30_39 + day.cases40_49 + day.cases50_59 + day.cases60_69 + day.cases70_79 + day.cases80_89 + day.cases90plus
+            f.write(f"{day.date},{day.province.name},{total_cases}\n")
+        f.close()
+
+        f = open("measuremeter/static/csv/bel14_july.csv", "w")
+        start_date = date(2020, 7, 1)
+        cases = BELCases.objects.filter(date__gte=start_date).order_by("date")
+        f.write("Date,Province,Value\n")
+        for day  in cases:
+            total_cases = day.cases0_9 + day.cases10_19 + day.cases20_29 + day.cases30_39 + day.cases40_49 + day.cases50_59 + day.cases60_69 + day.cases70_79 + day.cases80_89 + day.cases90plus
+            f.write(f"{day.date},{day.province.name},{total_cases}\n")
         f.close()
 
         f = open("measuremeter/static/csv/countries14_start.csv", "w")
