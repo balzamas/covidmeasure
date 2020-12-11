36c6a59541fe434265c0e2944789978e42308a5b
dbe_macix <d.berger@dontsniff.co.uk>
2020-12-11 19:20:23 +0100

    R in ranking

--- measuremeterdata/views/views_ranking_ch.py
+++ measuremeterdata/views/views_ranking_ch.py
@@ -1,4 +1,5 @@
 from measuremeterdata.models.models_ch import CHCases, CHCanton, CHMeasure
+from measuremeterdata.models.models import CasesDeaths
 from django.shortcuts import get_object_or_404, render
 from datetime import date, timedelta
 from django.template import loader
@@ -9,12 +10,29 @@ from django.db.models import F, Func, Q
 def ranking7_calc(cantons):
     canton_vals = []
 
+    ch_incidences = CasesDeaths.objects.filter(country=1, date__range=[date.today() - timedelta(days=5), date.today()]).order_by("-date")
+    ch_incidence = ch_incidences[0].cases_past7days
+
     for canton in cantons:
         date_tocheck = date.today()
 
-        cases = CHCases.objects.filter(canton=canton, date__range=[date_tocheck - timedelta(days=10), date_tocheck]).order_by("-date")
+        cases = CHCases.objects.filter(canton=canton, date__range=[date_tocheck - timedelta(days=25), date_tocheck]).order_by("-date")
 
         try:
+            if canton.level == 0:
+                r0 = 0
+                r0_date = date.today()
+                r_count = 0
+                r_under_one = True
+                for case in cases:
+                    if case.r0median:
+                        if r_count == 0:
+                            r0 = case.r0median
+                            r0_date = case.date
+                        if r_count < 7 and case.r0median > 1:
+                            r_under_one = False
+                        r_count += 1
+
             if (canton.level == 0 and cases[0].date == date.today()):
                 last_date = cases[2].date
                 last_prev7 = cases[2].incidence_past7days
@@ -49,25 +67,22 @@ def ranking7_calc(cantons):
 
                 score = 0 - cases[0].incidence_past7days - (last_tendency * 2)
 
-            score_7days_before = 0 - case_7days_before.incidence_past7days - (case_7days_before.development7to7 * 2)
         except:
             score = -99999
             score_14days_before = -99999
 
-        if (score > score_7days_before):
-            arrow = "arrow circle up green"
-        elif (score == score_7days_before):
-            arrow = "arrow circle left orange"
-        else:
-            arrow = "arrow circle down red"
+        incidence_below_ch = False
+        if last_prev7 < ch_incidence:
+            incidence_below_ch = True
 
         measures = CHMeasure.objects.filter(canton=canton).filter(Q(end__gte=date.today())|Q(end__isnull=True)).filter(Q(start__lte=date.today())).order_by("type__name")
 
-        canton_toadd = {"name": canton.name, "score": int(score), "score_before": int(score_7days_before),
-                        "date": last_date, "code": canton.code,
+        canton_toadd = {"name": canton.name, "score": int(score),
+                        "date": last_date, "code": canton.code, "level": canton.level,
                         "cur_prev": last_prev7, "cur_prev14": last_prev14, "tendency": last_tendency,
                         "cur_prev7": case_7days_before.incidence_past7days, "id": canton.swisstopo_id,
-                        "level": canton.level, "icon": arrow, "measures": measures}
+                        "level": canton.level, "measures": measures, "incidence_below_ch": incidence_below_ch,
+                        "r0":r0, "r0_date":r0_date, "r_under_one":r_under_one}
 
         canton_vals.append(canton_toadd)
 
@@ -103,6 +118,7 @@ def ranking14_calc(cantons):
 
         cases = CHCases.objects.filter(canton=canton,
                                        date__range=[date_tocheck - timedelta(days=10), date_tocheck]).order_by("-date")
+
 
         try:
             if (canton.level == 0 and cases[0].date == date.today()):
