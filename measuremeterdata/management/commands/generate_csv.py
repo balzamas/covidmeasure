d5e91f518c7186486e408a5ae970957aea7af394
David Berger <d.berger@dontsniff.co.uk>
2021-01-02 22:04:51 +0100

    New barraces

--- measuremeterdata/management/commands/generate_csv.py
+++ measuremeterdata/management/commands/generate_csv.py
@@ -6,6 +6,14 @@ from datetime import date
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        f = open("measuremeter/static/csv/cantons7_october.csv", "w")
+        start_date = date(2020, 10, 1)
+        cases = CHCases.objects.filter(canton__level=0, date__gte=start_date).order_by("date")
+        f.write("Date,Canton,Value\n")
+        for day  in cases:
+            f.write(f"{day.date},{day.canton.name},{day.incidence_past7days}\n")
+        f.close()
+
         f = open("measuremeter/static/csv/cantons14_start.csv", "w")
         start_date = date(2020, 2, 28)
         cases = CHCases.objects.filter(canton__level=0, date__gte=start_date).order_by("date")
@@ -56,6 +64,14 @@ class Command(BaseCommand):
             f.write(f"{day.date},{day.country.name},{day.cases_past14days}\n")
         f.close()
 
+        f = open("measuremeter/static/csv/countries14_october.csv", "w")
+        start_date = date(2020, 10, 1)
+        cases = CasesDeaths.objects.filter(country__continent=1,date__gte=start_date).order_by("date")
+        f.write("Date,Country,Value\n")
+        for day  in cases:
+            f.write(f"{day.date},{day.country.name},{day.cases_past14days}\n")
+        f.close()
+
         f = open("measuremeter/static/csv/countries7_start.csv", "w")
         start_date = date(2020, 2, 25)
         cases = CasesDeaths.objects.filter(country__continent=1,date__gte=start_date).order_by("date")
@@ -66,6 +82,14 @@ class Command(BaseCommand):
 
         f = open("measuremeter/static/csv/countries7_july.csv", "w")
         start_date = date(2020, 7, 1)
+        cases = CasesDeaths.objects.filter(country__continent=1,date__gte=start_date).order_by("date")
+        f.write("Date,Country,Value\n")
+        for day  in cases:
+            f.write(f"{day.date},{day.country.name},{day.cases_past7days}\n")
+        f.close()
+
+        f = open("measuremeter/static/csv/countries7_october.csv", "w")
+        start_date = date(2020, 10, 1)
         cases = CasesDeaths.objects.filter(country__continent=1,date__gte=start_date).order_by("date")
         f.write("Date,Country,Value\n")
         for day  in cases:
