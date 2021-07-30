37773224e1c48fcf61a31579cce8b70e56b9d859
balzamas <d.berger@dontsniff.co.uk>
2021-07-30 17:22:06 +0200

    New Vacc speed

--- measuremeterdata/management/commands/generate_csv.py
+++ measuremeterdata/management/commands/generate_csv.py
@@ -6,63 +6,95 @@ from datetime import date
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        f = open("measuremeter/static/csv/cantons7_october.csv", "w")
-        start_date = date(2020, 10, 1)
-        cases = CHCases.objects.filter(canton__level=0, date__gte=start_date).order_by("date")
-        f.write("Date,Canton,Value\n")
-        for day  in cases:
-            f.write(f"{day.date},{day.canton.name},{day.incidence_past7days}\n")
-        f.close()
+        try:
+            f = open("measuremeter/static/csv/cantons7_october.csv", "w")
+            start_date = date(2020, 10, 1)
+            cases = CHCases.objects.filter(canton__level=0, date__gte=start_date).order_by("date")
+            f.write("Date,Canton,Value\n")
+            for day  in cases:
+                f.write(f"{day.date},{day.canton.name},{day.incidence_past7days}\n")
+            f.close()
+        except:
+            pass
 
-        f = open("measuremeter/static/csv/cantons14_start.csv", "w")
-        start_date = date(2020, 2, 28)
-        cases = CHCases.objects.filter(canton__level=0, date__gte=start_date).order_by("date")
-        f.write("Date,Canton,Value\n")
-        for day  in cases:
-            f.write(f"{day.date},{day.canton.name},{day.incidence_past14days}\n")
-        f.close()
+        try:
+            f = open("measuremeter/static/csv/cantons14_start.csv", "w")
+            start_date = date(2020, 2, 28)
+            cases = CHCases.objects.filter(canton__level=0, date__gte=start_date).order_by("date")
+            f.write("Date,Canton,Value\n")
+            for day  in cases:
+                f.write(f"{day.date},{day.canton.name},{day.incidence_past14days}\n")
+            f.close()
+        except:
+            pass
 
-        f = open("measuremeter/static/csv/cantons14_july.csv", "w")
-        start_date = date(2020, 7, 1)
-        cases = CHCases.objects.filter(canton__level=0, date__gte=start_date).order_by("date")
-        f.write("Date,Canton,Value\n")
-        for day  in cases:
-            f.write(f"{day.date},{day.canton.name},{day.incidence_past14days}\n")
-        f.close()
+        try:
+            f = open("measuremeter/static/csv/cantons14_july.csv", "w")
+            start_date = date(2020, 7, 1)
+            cases = CHCases.objects.filter(canton__level=0, date__gte=start_date).order_by("date")
+            f.write("Date,Canton,Value\n")
+            for day  in cases:
+                f.write(f"{day.date},{day.canton.name},{day.incidence_past14days}\n")
+            f.close()
+        except:
+            pass
 
-        f = open("measuremeter/static/csv/bel14_start.csv", "w")
-        start_date = date(2020, 2, 28)
-        cases = BELCases.objects.filter(date__gte=start_date).order_by("date")
-        f.write("Date,Province,Value\n")
-        for day  in cases:
-            total_cases = day.cases0_9 + day.cases10_19 + day.cases20_29 + day.cases30_39 + day.cases40_49 + day.cases50_59 + day.cases60_69 + day.cases70_79 + day.cases80_89 + day.cases90plus
-            f.write(f"{day.date},{day.province.name},{total_cases}\n")
-        f.close()
+        try:
+            f = open("measuremeter/static/csv/bel14_start.csv", "w")
+            start_date = date(2020, 2, 28)
+            cases = BELCases.objects.filter(date__gte=start_date).order_by("date")
+            f.write("Date,Province,Value\n")
+            for day  in cases:
+                total_cases = day.cases0_9 + day.cases10_19 + day.cases20_29 + day.cases30_39 + day.cases40_49 + day.cases50_59 + day.cases60_69 + day.cases70_79 + day.cases80_89 + day.cases90plus
+                f.write(f"{day.date},{day.province.name},{total_cases}\n")
+            f.close()
+        except:
+            pass
 
-        f = open("measuremeter/static/csv/bel14_july.csv", "w")
-        start_date = date(2020, 7, 1)
-        cases = BELCases.objects.filter(date__gte=start_date).order_by("date")
-        f.write("Date,Province,Value\n")
-        for day  in cases:
-            total_cases = day.cases0_9 + day.cases10_19 + day.cases20_29 + day.cases30_39 + day.cases40_49 + day.cases50_59 + day.cases60_69 + day.cases70_79 + day.cases80_89 + day.cases90plus
-            f.write(f"{day.date},{day.province.name},{total_cases}\n")
-        f.close()
+        try:
+            f = open("measuremeter/static/csv/bel14_july.csv", "w")
+            start_date = date(2020, 7, 1)
+            cases = BELCases.objects.filter(date__gte=start_date).order_by("date")
+            f.write("Date,Province,Value\n")
+            for day  in cases:
+                total_cases = day.cases0_9 + day.cases10_19 + day.cases20_29 + day.cases30_39 + day.cases40_49 + day.cases50_59 + day.cases60_69 + day.cases70_79 + day.cases80_89 + day.cases90plus
+                f.write(f"{day.date},{day.province.name},{total_cases}\n")
+            f.close()
+        except:
+            pass
 
-        f = open("measuremeter/static/csv/countries14_start.csv", "w")
-        start_date = date(2020, 2, 25)
-        cases = CasesDeaths.objects.filter(country__continent=1,date__gte=start_date).order_by("date")
-        f.write("Date,Country,Value\n")
-        for day  in cases:
-            f.write(f"{day.date},{day.country.name},{day.cases_past14days}\n")
-        f.close()
+        try:
+            f = open("measuremeter/static/csv/countries14_start.csv", "w")
+            start_date = date(2020, 2, 25)
+            cases = CasesDeaths.objects.filter(country__continent=1,date__gte=start_date).order_by("date")
+            f.write("Date,Country,Value\n")
+            for day  in cases:
+                f.write(f"{day.date},{day.country.name},{day.cases_past14days}\n")
+            f.close()
+        except:
+            pass
 
-        f = open("measuremeter/static/csv/countries14_july.csv", "w")
-        start_date = date(2020, 7, 1)
-        cases = CasesDeaths.objects.filter(country__continent=1,date__gte=start_date).order_by("date")
-        f.write("Date,Country,Value\n")
-        for day  in cases:
-            f.write(f"{day.date},{day.country.name},{day.cases_past14days}\n")
-        f.close()
+        try:
+            f = open("measuremeter/static/csv/countries14_july.csv", "w")
+            start_date = date(2020, 7, 1)
+            cases = CasesDeaths.objects.filter(country__continent=1,date__gte=start_date).order_by("date")
+            f.write("Date,Country,Value\n")
+            for day  in cases:
+                f.write(f"{day.date},{day.country.name},{day.cases_past14days}\n")
+            f.close()
+        except:
+            pass
+
+        try:
+            f = open("measuremeter/static/csv/countries14_june21.csv", "w")
+            start_date = date(2021, 6, 1)
+            cases = CasesDeaths.objects.filter(country__continent=1,date__gte=start_date).order_by("date")
+            f.write("Date,Country,Value\n")
+            for day  in cases:
+                f.write(f"{day.date},{day.country.name},{day.cases_past14days}\n")
+            f.close()
+        except:
+            pass
 
         f = open("measuremeter/static/csv/countries14_october.csv", "w")
         start_date = date(2020, 10, 1)
@@ -82,6 +114,14 @@ class Command(BaseCommand):
 
         f = open("measuremeter/static/csv/countries7_july.csv", "w")
         start_date = date(2020, 7, 1)
+        cases = CasesDeaths.objects.filter(country__continent=1,date__gte=start_date).order_by("date")
+        f.write("Date,Country,Value\n")
+        for day  in cases:
+            f.write(f"{day.date},{day.country.name},{day.cases_past7days}\n")
+        f.close()
+
+        f = open("measuremeter/static/csv/countries7_june21.csv", "w")
+        start_date = date(2021, 6, 1)
         cases = CasesDeaths.objects.filter(country__continent=1,date__gte=start_date).order_by("date")
         f.write("Date,Country,Value\n")
         for day  in cases:
