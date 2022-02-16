8f1dac6334287e85022d022251cf8a3c1c223ce6
balzamas <d.berger@dontsniff.co.uk>
2022-02-16 19:28:25 +0100

    pimp Covidmeter

--- measuremeterdata/tasks/socialmedia/tweet_lovebot.py
+++ measuremeterdata/tasks/socialmedia/tweet_lovebot.py
@@ -24,6 +24,6 @@ def send_telegram():
 
     bot = telepot.Bot(settings.TELEGRAM_TOKEN_LOVE)
     print(bot.getMe())
-    #bot.sendMessage(settings.TELEGRAM_CHATID_LOVE, f"Hoi du")
+    bot.sendMessage(settings.TELEGRAM_CHATID_LOVE, f"Alles gute zum 40. Geburtstag mein liebstes Otterweibchen! ")
     bot.sendPhoto(settings.TELEGRAM_CHATID_LOVE, photo=open("/app/measuremeter/static/images/love/" + img, 'rb'))
 
